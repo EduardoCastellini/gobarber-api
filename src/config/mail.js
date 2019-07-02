65fb3a10f27bc8f012e28e70f2737c842ec5d1e2
Eduardo Castellini <eduardo.castellini@hotmail.com>
2019-07-01 21:23:44 -0300

    configurando envio de email de cancelamento

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '57d17a3f723b53',
+    pass: '954407d2645d8e',
+  },
+  default: {
+    from: 'Equipe GoBaber <noreply@gobarber.com.br>',
+  },
+};
